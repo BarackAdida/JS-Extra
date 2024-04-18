c362346d25be0ead6e9b30d2607ae7d5775a5a3e
Adida <adida.barack@student.moringaschool.com>
2024-04-18 13:03:12 +0300

    Further understanding of JS

---
+++ script.js
@@ -0,0 +1,28 @@
+const save = document.querySelector('#controvasia')
+const pChange = document.querySelector('#p1')
+// const baseUrl = 'http://localhost:3000/'
+// console.log(button)
+
+// function theClick() {
+//     alert('You clicked me!')
+//     pChange.style.fontSize = "30px";
+//     pChange.style.color = "#B3B6B7"
+// }
+function addStudentData() {
+    const data = {
+        name: 'Barack',
+        age: 18,
+        class: 'Infernals',
+        course: 'MoringaInstitute'
+    }
+    fetch(`http://localhost:3000/student`, {
+        method: 'POST',
+        headers: {
+            'content-type': 'aplication/json',
+        },
+        body: 'JSON.stringify(data)'
+    })
+    .then(Response => Response.json())
+    .then((data) => console.log(data))
+}
+save.addEventListener('click', addStudentData)
