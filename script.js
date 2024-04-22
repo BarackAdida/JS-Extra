a6c8cc7fef38aacd19f5126d2e7b3bd2a7131224
Adida <adida.barack@student.moringaschool.com>
2024-04-22 12:30:14 +0300

    Updated

--- script.js
+++ script.js
@@ -8,21 +8,47 @@ const pChange = document.querySelector('#p1')
 //     pChange.style.fontSize = "30px";
 //     pChange.style.color = "#B3B6B7"
 // }
-function addStudentData() {
-    const data = {
-        name: 'Barack',
-        age: 18,
-        class: 'Infernals',
-        course: 'MoringaInstitute'
-    }
-    fetch(`http://localhost:3000/student`, {
-        method: 'POST',
-        headers: {
-            'content-type': 'aplication/json',
-        },
-        body: 'JSON.stringify(data)'
-    })
-    .then(Response => Response.json())
-    .then((data) => console.log(data))
+// function addStudentData() {
+//     const data = {
+//         name: 'Barack',
+//         age: 18,
+//         class: 'Infernals',
+//         course: 'MoringaInstitute'
+//     }
+//     fetch(`http://localhost:3000/student`, {
+//         method: 'POST',
+//         headers: {
+//             'content-type': 'aplication/json',
+//         },
+//         body: 'JSON.stringify(data)'
+//     })
+//     .then(Response => Response.json())
+//     .then((data) => console.log(data))
+// }
+// save.addEventListener('click', addStudentData)
+
+// A calculator
+function display (value) {
+    var display= document.getElementById('display')
+    .value += value;
 }
-save.addEventListener('click', addStudentData)
+
+function deleteButton (value){
+    const valueOnDisplay = document.getElementById('display').value
+    document.getElementById('display')
+    .value = valueOnDisplay.slice(0, -1)
+}
+
+function calculate () {
+    var display = document.getElementById('display')
+   try {
+     display.value = eval(display.value)
+   }
+   catch (error) {
+    display.value = 'Error'
+   }
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+  }
